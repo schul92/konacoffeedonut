a78bf36cada77df23e5b07840cc114948cddfa09
Steve Song <steve.b.song92@gmail.com>
2025-10-01 20:51:17 -0400

    Use raw img tag for menu page to display menu.png directly

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,16 +1,11 @@
-import Image from 'next/image';
-
 export default function MenuPage() {
   return (
     <div className="min-h-screen bg-black flex items-center justify-center p-4">
       <div className="w-full max-w-[1920px]">
-        <Image
+        <img
           src="/menu.png"
           alt="Kona Coffee Donut Menu"
-          width={1920}
-          height={1080}
           className="w-full h-auto"
-          priority
         />
       </div>
     </div>
