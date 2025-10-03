b1f40ce93b33b6688c77010d36225341700e2d02
Steve Song <steve.b.song92@gmail.com>
2025-10-02 21:29:10 -0400

    simple imgs

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    unoptimized: false,
+    unoptimized: true,
     remotePatterns: [],
   },
   compress: true,
